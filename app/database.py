dc0afda7c343df2658a381c8d2d68de1154f9b0e
Artem <mihaylovartem54@gmail.com>
2024-12-10 01:39:15 +0300

    fix database settings

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+asyncpg://postgres:135892@localhost/fastapitest"
+DATABASE_URL='postgresql+asyncpg://postgres:password@db:5432/mydatabase'
 
 engine = create_async_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
